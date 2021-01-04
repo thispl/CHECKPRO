8caa4b1c56998dee795c54cbc46b5f771e584bb0
Abdulla P I <abdulla.pi@groupteampro.com>
2021-01-04 09:34:14 +0000

    Print Format

--- checkpro/checkpro/doctype/court_record_check/court_record_check.js
+++ checkpro/checkpro/doctype/court_record_check/court_record_check.js
@@ -5,4 +5,17 @@ frappe.ui.form.on('Court Record Check', {
 	// refresh: function(frm) {
 
 	// }
+	after_save:function(frm){
+		console.log("call")
+		frappe.call({
+			method:'checkpro.checkpro.doctype.court_record_check.court_record_check.set_epi',
+			args:{
+				'name': frm.doc.name,
+				"case_id": frm.doc.case_id
+			},
+			callback: function (r) {
+				console.log("hi")
+			}
+		})
+	}
 });
--- checkpro/checkpro/doctype/court_record_check/court_record_check.py
+++ checkpro/checkpro/doctype/court_record_check/court_record_check.py
@@ -3,8 +3,22 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class CourtRecordCheck(Document):
 	pass
+
+@frappe.whitelist()
+def set_epi(name,case_id):
+	frappe.errprint("called")
+	vpd = frappe.get_doc("Verify Court Record Check",{"case_id":case_id})
+	epi = frappe.get_doc("Court Record Check",{"name":name})
+	vpd.update({
+		"entry_social_behaviour": epi.social_behaviour
+	})
+	vpd.save(ignore_permissions=True)
+	frappe.db.commit()
+
+
+
